7b5b2f90fadadae6ac63c9a93f39c79e520a7204
Jakkrid Boonseela <jakkridboonseela@Jakkrids-MacBook-Air.local>
2023-03-03 14:48:53 +0700

    T2 add

--- html/t2-send.php
+++ html/t2-send.php
@@ -12,16 +12,17 @@ echo $std_id = $_SESSION['SES_EN_REG_USER'];
 echo $type_thesis_is = $_POST['type_thesis_is'];
 echo $type_file_url = $_POST['type_file_url'];
 if($type_file_url==1){
-    $typeThesis = strrchr($_FILES['FileThesis']['name'],".");
+    
 }else if($type_file_url==2){
     $urlThesis = $_POST['urlThesis'];
 }
-$type = strrchr($_FILES['FileT2']['name'],".");
-$typeSubmit = strrchr($_FILES['FileSubmit']['name'],".");
+
+
 if(isset($_POST['submit'])){
-    echo 55;
-if($typeThesis!=''){
-if($typeThesis == "png" || $typeThesis == "PNG" || $typeThesis == "PNG" || $typeThesis == "jpg" || $typeThesis == "jpeg"  || $typeThesis == "JPG" || $typeThesis == "docx" || $typeThesis == "DOCX" || $typeThesis == "doc" || $typeThesis == "DOC" || $typeThesis == "pdf" || $typeThesis == "PDF"  ){
+   
+if($_FILES['FileThesis']['name']){
+  echo   $typeThesis = strrchr($_FILES['FileThesis']['name'],".");
+if($typeThesis == ".png" || $typeThesis == ".PNG" || $typeThesis == ".PNG" || $typeThesis == ".jpg" || $typeThesis == ".jpeg"  || $typeThesis == ".JPG" || $typeThesis == ".docx" || $typeThesis == ".DOCX" || $typeThesis == ".doc" || $typeThesis == ".DOC" || $typeThesis == ".pdf" || $typeThesis == ".PDF"  ){
     $newname = $std_id."FileThesis".$date.'-'.$numrand.$typeThesis;
     $location_thesis="upload/".$newname; 
     $path_copy=$location_thesis.$newname;
@@ -32,8 +33,9 @@ if($typeThesis == "png" || $typeThesis == "PNG" || $typeThesis == "PNG" || $type
     //echo "<script> window.location ='javascript:history.go(-1)'; </script>"; // ไป กลับ form เดิม
  }
 }
-if($type!=''){
-if($type == "png" || $type== "PNG" || $type == "PNG" || $type == "jpg" || $type == "jpeg"  || $type == "JPG" || $type == "docx" || $type == "DOCX" || $type == "doc" || $type == "DOC" || $type == "pdf" || $type == "PDF"  ){
+if($_FILES['FileT2']['name']){
+   echo $type = strrchr($_FILES['FileT2']['name'],".");
+if($type == ".png" || $type== ".PNG" || $type == ".PNG" || $type == ".jpg" || $type == ".jpeg"  || $type == ".JPG" || $type == ".docx" || $type == ".DOCX" || $type == ".doc" || $type == ".DOC" || $type == ".pdf" || $type == ".PDF"  ){
     $newname = $std_id."FileT2".$date.'-'.$numrand.$type;
     $location_t2="upload/".$newname; 
     $path_copy=$location_t2.$newname;
@@ -45,12 +47,13 @@ if($type == "png" || $type== "PNG" || $type == "PNG" || $type == "jpg" || $type 
 }
 }
 
-if($typeSubmit!=''){
-    if($typeSubmit == "png" || $typeSubmit== "PNG" || $typeSubmit == "PNG" || $typeSubmit == "jpg" || $typeSubmit == "jpeg"  || $typeSubmit == "JPG" || $typeSubmit == "docx" || $typeSubmit == "DOCX" || $typeSubmit == "doc" || $typeSubmit == "DOC" || $typeSubmit == "pdf" || $typeSubmit == "PDF"  ){
+if($_FILES['FileSubmit']['name']){
+  echo  $typeSubmit = strrchr($_FILES['FileSubmit']['name'],".");
+    if($typeSubmit == ".png" || $typeSubmit== ".PNG" || $typeSubmit == ".PNG" || $typeSubmit == ".jpg" || $typeSubmit == ".jpeg"  || $typeSubmit == ".JPG" || $typeSubmit == ".docx" || $typeSubmit == ".DOCX" || $typeSubmit == ".doc" || $typeSubmit == ".DOC" || $typeSubmit == ".pdf" || $typeSubmit == ".PDF"  ){
         $newname = $std_id."FileSubmit".$date.'-'.$numrand.$typeSubmit;
         $location_submit="upload/".$newname; 
         $path_copy=$location_submit.$newname;
-        move_uploaded_file($_FILES['FileT2']['tmp_name'], $location_submit);
+        move_uploaded_file($_FILES['FileSubmit']['tmp_name'], $location_submit);
         $sql_up = "";
     }else{
       //  echo "<script> window.location ='javascript:history.go(-1)'; </script>"; // ไป กลับ form เดิม
@@ -65,7 +68,7 @@ if($typeSubmit!=''){
   }else{
     $file_url = $urlThesis ;
   }
-  echo $sql = "INSERT INTO info_t2 (std_id, type_send, fileT2,send_date,thesis_file_link,fileThesis,fileSend_note)VALUES ('$std_id', '$type_thesis_is', '$location_t2',Now(),'$file_url','$location_submit)";
+  echo $sql = "INSERT INTO info_t2 (std_id, type_send, fileT2,send_date,thesis_file_links,fileSend_note)VALUES ('$std_id', '$type_thesis_is', '$location_t2',Now(),'$file_url','$location_submit')";
   $rs = $mysqli->query($sql);
   echo "<script> window.location ='javascript:history.go(-1)'; </script>"; // ไป กลับ form เดิม
 }
